2072419389f2ecbc0cf268d55b6966c21f1d2a3f
Aanchalt14 <aanchalt71@gmail.com>
2021-01-13 12:11:18 +0530

    Add : Create contacts in AddressBook

--- AddressBook.java
+++ AddressBook.java
@@ -7,3 +7,19 @@ public class Addressbook {
     }
 
 }
+class CreateContacts {
+  String firstName, lastName, address, city, state;
+  long zip, phoneNumber;
+  int id;
+
+   public void AddressBook( String firstName, String lastName, String address, String city, String state, long zip,long phoneNumber) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.city = city;
+    this.state = state;
+    this.zip = zip;
+    this.phoneNumber = phoneNumber;
+ }
+}
+    
