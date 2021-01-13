06d2337407ac8df8a01ba6e0c3a55a99eba40bca
Aanchalt14 <aanchalt71@gmail.com>
2021-01-13 11:41:52 +0530

    Add : Welcome message

---
+++ AddressBook.java
@@ -0,0 +1,9 @@
+package Address.com;
+public class Addressbook {
+	
+    public static void main (String args[] ) {
+    System.out.println("Welcome to AddressBook program in AddressBook main class on master branch");    	
+   
+    }
+
+}
